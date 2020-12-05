92c0510cdf236ec712e403f8a4cedcdf8147de0c
Jayasurya <jsuryakt@gmail.com>
2020-12-05 19:21:58 +0530

    Change BASE_DIR path

--- todo/settings.py
+++ todo/settings.py
@@ -13,7 +13,7 @@ https://docs.djangoproject.com/en/3.1/ref/settings/
 from pathlib import Path
 import os
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = 'f+6t*zlokx7ibvc+q74@y#!(confk_v%nacpx5b9)5(e!e!ufq'

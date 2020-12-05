b771801b6e1377f0146e77aad98792d3cde19fe9
Jayasurya <jsuryakt@gmail.com>
2020-12-05 15:30:34 +0530

    Remove Secret Key

--- todo/settings.py
+++ todo/settings.py
@@ -19,9 +19,6 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'f+6t*zlokx7ibvc+q74@y#!(confk_v%nacpx5b9)5(e!e!ufq'
-
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 

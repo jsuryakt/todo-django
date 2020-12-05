34030a8465795130069d5381942f481941d49d74
Jayasurya <jsuryakt@gmail.com>
2020-12-05 12:44:09 +0530

    Add responsiveness

--- maketodo/views.py
+++ maketodo/views.py
@@ -14,29 +14,21 @@ def todo_view(request):
         form = ToDoForm(request.POST)
         if form.is_valid():
             text = form.cleaned_data.get('todo_text')
-            # text = request.POST.get('todo_text')
             ToDo.objects.create(todo_text = text)
     return redirect('home')
 
 def edit_view(request, pk):
-    # return HttpResponse("ho")
     todo_obj = ToDo.objects.get(pk = pk)
-    print(todo_obj)
     form = ToDoForm(instance = todo_obj)
     context = {'form':form,'todo_obj': todo_obj}
-    print(form)
     if request.method == 'POST':
         form = ToDoForm(request.POST, instance = todo_obj)
         if form.is_valid():
             form.save()
         return redirect('home')
     return render(request, 'edit.html', context)
-          
-
-
  
 def delete_view(request, pk):
-    # return HttpResponse("ho")
     if request.method == 'POST':
         todo_obj = ToDo.objects.get(pk = pk)
         todo_obj.delete()
